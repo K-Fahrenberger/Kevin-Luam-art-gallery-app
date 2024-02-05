d9758b5567b66640d56986a40558d32a94f8b0f4
Luam Belay <luam.belay@gmail.com>
2024-02-05 13:34:33 +0100

    User story 1 in progress

--- art-gallery-app/pages/index.js
+++ art-gallery-app/pages/index.js
@@ -1,7 +1,30 @@
+import useSWR from "swr";
+import Link from "next/link";
+import ArtPieces from "@/components/ArtPieces";
+
+const url = "https://example-apis.vercel.app/api/art";
+
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR(url);
+  console.log("data ", data);
+
+  // loading & error msg
+  if (isLoading)
+    return (
+      <h1 style={{ textAlign: "center", marginTop: "50vh" }}>Loading...</h1>
+    );
+  if (error)
+    return (
+      <h1 style={{ textAlign: "center", marginTop: "50vh" }}>
+        {error.message}
+      </h1>
+    );
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Gallery</h1>
+      <ArtPieces pieces={data} />
+      <Link href="/artpieces">Art Pieces</Link>
     </div>
   );
 }
