2ed37acbaa71b92e566faeace2d372bc2099ea07
Luam Belay <luam.belay@gmail.com>
2024-02-07 09:39:27 +0100

    Initial changes in FavoriteButton component

--- components/FavoriteButton/index.js
+++ components/FavoriteButton/index.js
@@ -1,20 +1,26 @@
 import Image from "next/image";
+import styled from "styled-components";
 import { ReactComponent as HeartFilled } from "public/resources/assets/heart-filled.svg";
 import { ReactComponent as HeartOutline } from "public/resources/assets/heart-outline.svg";
 
+const StyledButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
 export default function FavoriteButton({ isFavorite, onClick }) {
   return (
-    <button
+    <StyledButton
       className="favorite-button"
-      aria-label="Favorite this art piece"
+      aria-label="Save as favorite"
       onClick={onClick}
     >
       {isFavorite ? <HeartFilled /> : <HeartOutline />}
-      <Image
-        alt="heart"
-        width={20}
-        height={20}
-      />
-    </button>
+      <Image alt="heart" width={20} height={20} />
+    </StyledButton>
   );
 }
